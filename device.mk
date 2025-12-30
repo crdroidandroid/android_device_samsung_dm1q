60120172575556c2bc9ffb8889ed67f31180d221
Josip Kelecic <kelecicj@gmail.com>
2025-12-30 11:36:12 +0100

    dm1q: add lineage touch entry to non common devicetree

--- device.mk
+++ device.mk
@@ -31,6 +31,10 @@ PRODUCT_PACKAGES += \
     SystemUIResDm1q \
     WifiResTargetDm1q
 
+# Touch features
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch-service.samsung
+
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
     firmware_wlanmdsp.otaupdate_symlink \
